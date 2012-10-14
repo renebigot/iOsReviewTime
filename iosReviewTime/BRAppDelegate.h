be21d1058570b3dc1754f54314f1dc319d2a30a7
René BIGOT <bigotrene@gmail.com>
2012-10-14 23:16:20 +0200

    Initial Commit

---
+++ iosReviewTime/BRAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BRAppDelegate.h
+//  iosReviewTime
+//
+//  Created by René Bigot on 14/10/12.
+//  Copyright (c) 2012 René Bigot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BRViewController;
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BRViewController *viewController;
+
+@end
---
+++ iosReviewTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iosReviewTime
+//
+//  Created by René Bigot on 14/10/12.
+//  Copyright (c) 2012 René Bigot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
