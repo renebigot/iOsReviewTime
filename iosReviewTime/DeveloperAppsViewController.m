8bad6576c6549fa1a1cb1c57e106163f34cb215b
René Bigot <bigotrene@me.com>
2014-01-01 13:07:31 +0100

    Multi-dev issue fix

--- iosReviewTime/DeveloperAppsViewController.m
+++ iosReviewTime/DeveloperAppsViewController.m
@@ -3,7 +3,7 @@
 //  iOSReviewTime
 //
 //  Created by iRare Media on 12/20/13.
-//  Copyright (c) 2013 iRare Media. All rights reserved.
+//  Copyright (c) 2012 René Bigot. Copyright (c) 2013 iRare Media. All rights reserved.
 //
 
 #import "DeveloperAppsViewController.h"
@@ -13,7 +13,7 @@
 } @end
 
 @implementation DeveloperAppsViewController
-@synthesize developerName, tableViewCells, dictionaryData, appURLs;
+@synthesize developerNameOrId, tableViewCells, dictionaryData, appURLs;
 @synthesize loginView, activityIndicator, tableview;
 @synthesize loginButton, refreshButton;
 
@@ -27,16 +27,17 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
-    developerName = [FDKeychain itemForKey:@"developerName" forService:@"iOSReviewTime" error:nil];
+
+    developerNameOrId = [FDKeychain itemForKey:@"developerName" forService:@"iOSReviewTime" error:nil];
     
     if (didPresentLogin) {
-        if (developerName != NULL) {
+        if (developerNameOrId != NULL) {
             loginButton.title = NSLocalizedString(@"Logout", @"Button Title");
-            [self refreshApps];
+            if ([developerNameOrId integerValue] > 0)
+                [self refreshApps];
         }
     } else {
-        if (developerName != NULL) {
+        if (developerNameOrId != NULL) {
             loginButton.title = NSLocalizedString(@"Logout", @"Button Title");
             [self refreshApps];
         } else {
@@ -58,7 +59,7 @@
 }
 
 - (void)didLoginUser {
-    if (developerName != NULL) {
+    if (developerNameOrId != NULL) {
         loginButton.title = NSLocalizedString(@"Logout", @"Button Title");
     } else {
         loginButton.title = NSLocalizedString(@"Login", @"Button Title");
@@ -79,10 +80,20 @@
 - (IBAction)refreshApps {
     refreshButton.enabled = NO;
     
-    developerName = [FDKeychain itemForKey:@"developerName" forService:@"iOSReviewTime" error:nil];
-    NSString *formattedName = [developerName stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSString *requestURL = [NSString stringWithFormat:@"http://itunes.apple.com/search?term=%@&media=software&lang=en_US&country=US", formattedName];
-    if (developerName == nil) {
+    developerNameOrId = [FDKeychain itemForKey:@"developerName" forService:@"iOSReviewTime" error:nil];
+    
+    NSString *requestURL = nil;
+    //Searching developer ID ?
+    if ([developerNameOrId integerValue] > 0) {
+        _developerScreenName = nil;
+        requestURL = [NSString stringWithFormat:@"http://itunes.apple.com/lookup?id=%@&entity=software", developerNameOrId];
+    } else {
+        _developerScreenName = developerNameOrId;
+        NSString *formattedName = [developerNameOrId stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        requestURL = [NSString stringWithFormat:@"http://itunes.apple.com/search?term=%@&media=software&lang=en_US&country=US", formattedName];
+    }
+    
+    if (developerNameOrId == nil) {
         [self login];
         return;
     }
@@ -95,8 +106,10 @@
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     [manager GET:requestURL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        dictionaryData = responseObject;
+        _developersArray = [[NSMutableArray alloc] init];
         
+        dictionaryData = responseObject;
+
         // Loop through the "results" array inside the returned dictionary - get each dictionary result inside of the results array
         for (NSDictionary *result in [dictionaryData objectForKey:@"results"]) {
             // Check if the listing matches the correct listing kind (ex. apps, not songs or movies). Use the "kind" tag and check for "software" value
@@ -122,6 +135,14 @@
             // NSString *appID = [result objectForKey:@"bundleId"];
             NSString *appIcon = [result objectForKey:@"artworkUrl100"]; // Can also request a 60x60 px version or a 512x512 px version
             NSString *appURL = [result objectForKey:@"trackViewUrl"];
+
+            NSDictionary *tmpArtistDict = @{@"name": result[@"artistName"], @"id": [result[@"artistId"] stringValue]};
+            if (!_developerScreenName) {
+                _developerScreenName = result[@"artistName"];
+            } else if (![_developersArray containsObject:tmpArtistDict]) {
+                [_developersArray addObject:tmpArtistDict];
+            }
+            
             // NSString *appScreenshot = [[result objectForKey:@"screenshotUrls"] firstObject];
             [appURLs addObject:appURL];
             
@@ -129,7 +150,21 @@
             UITableViewCell *cell = [self.tableview dequeueReusableCellWithIdentifier:CellIdentifier];
             if (cell == nil) cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
             
-            [cell.imageView setImageWithURL:[NSURL URLWithString:appIcon]]; // placeholderImage:[UIImage imageNamed:@"appglobe"]];
+//            [cell.imageView setImageWithURL:[NSURL URLWithString:appIcon]]; // placeholderImage:[UIImage imageNamed:@"appglobe"]];
+            cell.imageView.image = [UIImage imageNamed:@"iosGrid"];
+            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
+            dispatch_async(queue, ^(void) {
+                NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:appIcon]];
+                UIImage* image = [[UIImage alloc] initWithData:imageData];
+                if (image) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        cell.imageView.image = image;
+                        [cell setNeedsLayout];
+                    });
+                }
+            });
+            
+            
             // [cell.imageView setFrame:CGRectMake(cell.imageView.frame.origin.x, cell.imageView.frame.origin.x, 50, 50)];
             // [cell.imageView.layer setCornerRadius:4];
             // [cell.imageView.layer setRasterizationScale:[UIScreen mainScreen].scale];
@@ -140,9 +175,29 @@
         }
         
         [tableview reloadData];
+        [tableview setScrollIndicatorInsets:UIEdgeInsetsMake(0., 0., 50., 0.)];
+        [tableview setContentInset:UIEdgeInsetsMake(0., 0., 50., 0.)];
+        
         [activityIndicator stopAnimating];
         [activityIndicator setHidden:YES];
         refreshButton.enabled = YES;
+        
+        if ([_developersArray count] > 1) {
+            UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:[NSString stringWithFormat:@"Many results found for \"%@\" found. Which one do you want to use ?", developerNameOrId]
+                                                                     delegate:self
+                                                            cancelButtonTitle:nil
+                                                       destructiveButtonTitle:nil
+                                                            otherButtonTitles:nil];
+            for (NSDictionary *developerDict in _developersArray) {
+                [actionSheet addButtonWithTitle:developerDict[@"name"]];
+            }
+            [actionSheet addButtonWithTitle:@"Cancel"];
+            [actionSheet setCancelButtonIndex:[actionSheet numberOfButtons] - 1];
+
+            didPresentLogin = YES;
+            [actionSheet showFromTabBar:self.tabBarController.tabBar];
+        }
+
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
         [activityIndicator stopAnimating];
@@ -158,14 +213,17 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    developerName = [FDKeychain itemForKey:@"developerName" forService:@"iOSReviewTime" error:nil];
-    if (developerName == NULL) return 0;
+    developerNameOrId = [FDKeychain itemForKey:@"developerName" forService:@"iOSReviewTime" error:nil];
+    if (developerNameOrId == NULL) return 0;
     else return [tableViewCells count];
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    if (developerName != NULL) return [NSString stringWithFormat:NSLocalizedString(@"Apps created by %@", nil), developerName];
-    else return NSLocalizedString(@"To tweet about your app review times, tap the login button and type your Organization / Team name", nil);
+    if (developerNameOrId != NULL) {
+        return [NSString stringWithFormat:NSLocalizedString(@"Apps created by %@", nil), _developerScreenName];
+    } else {
+        return NSLocalizedString(@"To tweet about your app review times, tap the login button and type your Organization / Team name", nil);
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -187,6 +245,20 @@
     [controller addImage:[[tableViewCells objectAtIndex:indexPath.row] imageView].image];
     
     [self presentViewController:controller animated:YES completion:nil];
+}
+
+#pragma mark - UIActionSheet Delegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex > [_developersArray count])
+        return;
+
+    [FDKeychain saveItem:_developersArray[buttonIndex][@"id"]
+                  forKey:@"developerName"
+              forService:@"iOSReviewTime"
+                   error:nil];
+    
+    [self refreshApps];
 }
 
 @end
