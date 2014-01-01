16a534e18eb473711e878371ef3a1b096b1d27ab
René Bigot <bigotrene@me.com>
2014-01-01 13:06:58 +0100

    Multi-dev issue fix

--- iosReviewTime/DeveloperAppsViewController.h
+++ iosReviewTime/DeveloperAppsViewController.h
@@ -3,7 +3,7 @@
 //  iOSReviewTime
 //
 //  Created by iRare Media on 12/20/13.
-//  Copyright (c) 2013 iRare Media. All rights reserved.
+//  Copyright (c) 2012 René Bigot. Copyright (c) 2013 iRare Media. All rights reserved.
 //
 
 #import "AFNetworking.h"
@@ -11,9 +11,13 @@
 #import "LoginViewController.h"
 #import "BRAppCell.h"
 
-@interface DeveloperAppsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIBarPositioningDelegate, LoginViewControllerDelegate>
+@interface DeveloperAppsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIBarPositioningDelegate, LoginViewControllerDelegate, UIActionSheetDelegate> {
+    
+    NSString *_developerScreenName;
+    NSMutableArray *_developersArray;
+}
 
-@property (strong, nonatomic) NSString *developerName;
+@property (strong, nonatomic) NSString *developerNameOrId;
 @property (strong, nonatomic) NSDictionary *dictionaryData;
 @property (strong, nonatomic) NSMutableArray *tableViewCells;
 @property (strong, nonatomic) NSMutableArray *appURLs;
